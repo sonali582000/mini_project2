a76d8ea39e4368881a287c1a24226eadc1d07d69
sonali582000 <rathodsonali393@gmail.com>
2024-01-19 17:23:29 +0100

    Added isAuthenticated in users

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,15 +1,18 @@
-const User = require("../models/User.model")
+require("dotenv").config();
+const { isAuthenticated } = require("../middleware/route-guard.middleware");
+const User = require("../models/User.model");
 const router = require("express").Router();
 
-router.get("/:id", async(req, res)=> {
-    const {id} = req.params;
-    try {
-        const user = await User.findById(id)
-    res.status(200).json(user)
-    } catch (error) {
-        res.status(500).json({error: "error occurs"})
-    }
-    
-})
+router.get("/:id", isAuthenticated, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const currentUser = await User.findById(req.tokenPayload.userId);
+    res.status(200).json(currentUser);
+    // const user = await User.findById(id);
+    // res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "error occurs" });
+  }
+});
 
 module.exports = router;
